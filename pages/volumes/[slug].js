b3d880e228d228f65184200c6935baa74b8c63db
Niko Cremer <n.ko@posteo.de>
2024-01-22 12:27:23 +0100

    add svg arrows

--- pages/volumes/[slug].js
+++ pages/volumes/[slug].js
@@ -2,6 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { volumes } from "../../lib/data";
+import styled, { css } from "styled-components";
+import right from "@/public/images/arrow-right.svg";
+import left from "@/public/images/arrow-left.svg";
 
 export default function VolumeDetail() {
   const router = useRouter();
@@ -17,37 +20,46 @@ export default function VolumeDetail() {
     return null;
   }
 
-  const { title, description, cover, books } = volume;
+  const { title, description, cover, books, color } = volume;
+  console.log("color", color);
+  const StyledSection = styled.div`
+    background-color: ${(prop) =>
+      prop.$backgroundcolor === "white" ? "white" : prop.$backgroundcolor};
+  `;
 
   return (
     <>
       <Link href="/volumes">← All Volumes</Link>
       <h1>{title}</h1>
       <p>{description}</p>
-      <ul>
-        {books.map(({ ordinal, title }) => (
-          <li key={title}>
-            {ordinal}: <strong>{title}</strong>
-          </li>
-        ))}
-      </ul>
-      <Image
-        src={cover}
-        alt={`Cover image of ${title}`}
-        width={140}
-        height={230}
-      />
+      <StyledSection $backgroundcolor={color}>
+        <ul>
+          {books.map(({ ordinal, title }) => (
+            <li key={title}>
+              {ordinal}: <strong>{title}</strong>
+            </li>
+          ))}
+        </ul>
+        <Image
+          src={cover}
+          alt={`Cover image of ${title}`}
+          width={140}
+          height={230}
+        />
+      </StyledSection>
       {previousVolume ? (
         <div>
           <Link href={`/volumes/${previousVolume.slug}`}>
-            ← Previous Volume: {previousVolume.title}
+            <Image src={left} alt="next page" width={20} height={20} /> Previous
+            Volume: {previousVolume.title}
           </Link>
         </div>
       ) : null}
       {nextVolume ? (
         <div>
           <Link href={`/volumes/${nextVolume.slug}`}>
-            Next Volume: {nextVolume.title} →
+            Next Volume: {nextVolume.title}{" "}
+            <Image src={right} alt="next page" width={20} height={20} />
           </Link>
         </div>
       ) : null}
